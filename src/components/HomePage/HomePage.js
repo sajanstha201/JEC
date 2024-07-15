4fb1e197f0b50a86d539449a9bf1c06ab0edac22
aakriti <aakritip512@gmail.com>
2024-07-15 15:50:00 +0545

    academic program heading

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,12 +1,15 @@
 import civil from "../../media/images/civil.jpg";
 import becomputer from "../../media/images/computer.jpg";
 import electronics from "../../media/images/electronics.jpg";
-import uniimage from '../../media/images/uniimg.jpg';
+import uniimage from "../../media/images/uniimg.jpg";
 
 export const HomePage = () => {
   return (
     <>
-    {/* our programs sections */}
+      <div className="course-titles flex justify-center my-5 text-4xl font-extrabold text-slate-400 md:text-6xl">
+        Academic Programs
+      </div>
+      {/* our programs sections */}
       <div className="course-desc w-[94%] flex flex-col sm:flex-row justify-center mx-auto gap-6">
         <div className="course1 relative bg-transparent-black my-2 md:w-1/3">
           <img
@@ -94,37 +97,61 @@ export const HomePage = () => {
 
       {/* why choose section */}
       <div className="w-[94%] mx-auto flex justify-between md:flex-row flex-col md:mt-16">
-            {/* <!-- why choose us text --> */}
-            <div className="md:w-[40%] w-full md:mt-28 md:mb-0">
-                <h1 className="why-choose text-5xl text-blue-600 font-bold">Why Choose <span
-                        className="text-red-700 font-bold">JEC</span></h1>
-                <p className="text-slate-800 text-lg">Our college offers three undergraduate programs in Civil Engineering, <span
-                        className="text-red-700">Computer Engineering</span>, and <span className="text-red-700">Electronics and
-                        Information Engineering</span>, providing students with a well-rounded education</p>
-                <button
-                    className="px-3 py-2 text-xl text-blue-500 hover:bg-blue-500 hover:text-white ease-in duration-100 border border-blue-500 my-1">MORE
-                    ABOUT JEC</button>
-            </div>
-
-            {/* <!-- images section --> */}
-            <div className="md:w-[60%] w-full flex flex-wrap md:flex-nowrap justify-between md:gap-9">
-                <div className="w-full md-w-[48%] mb-4 md:mb-0">
-                    <div className="h-auto rounded-lg my-1 bg-slate-100 shadow-gray-300">
-                        <img src={uniimage} className="w-full h-auto rounded-lg shadow-md" alt=""/>
-                        <p className="text-center mt-2 hover:text-blue-500">Scholarship Schemes</p>
-                    </div>
-                    <div className="h-auto rounded-lg my-1 bg-slate-100 shadow-gray-300">
-                        <img src={uniimage} className="w-full h-auto rounded-lg shadow-md" alt=""/>
-                        <p className="text-center mt-2 hover:text-blue-500">Prime Location</p>
-                    </div>
-                </div>
-                <div className="w-full md-w-[48%] md:mt-24 ">
-                    <img src={uniimage}
-                        className="w-full h-auto rounded-lg shadow-md my-1 bg-slate-100 shadow-gray-300" alt=""/>
-                    <p className="text-center mt-2 hover:text-blue-500">Internship Facilities</p>
-                </div>
-            </div>
+        {/* <!-- why choose us text --> */}
+        <div className="md:w-[40%] w-full md:mt-28 md:mb-0">
+          <h1 className="why-choose text-5xl text-blue-600 font-bold">
+            Why Choose <span className="text-red-700 font-bold">JEC</span>
+          </h1>
+          <p className="text-slate-800 text-lg">
+            Our college offers three undergraduate programs in Civil
+            Engineering,{" "}
+            <span className="text-red-700">Computer Engineering</span>, and{" "}
+            <span className="text-red-700">
+              Electronics and Information Engineering
+            </span>
+            , providing students with a well-rounded education
+          </p>
+          <button className="px-3 py-2 text-xl text-blue-500 hover:bg-blue-500 hover:text-white ease-in duration-100 border border-blue-500 my-1">
+            MORE ABOUT JEC
+          </button>
         </div>
+
+        {/* <!-- images section --> */}
+        <div className="md:w-[60%] w-full flex flex-wrap md:flex-nowrap justify-between md:gap-9">
+          <div className="w-full md-w-[48%] mb-4 md:mb-0">
+            <div className="h-auto rounded-lg my-1 bg-slate-100 shadow-gray-300">
+              <img
+                src={uniimage}
+                className="w-full h-auto rounded-lg shadow-md"
+                alt=""
+              />
+              <p className="text-center mt-2 hover:text-blue-500">
+                Scholarship Schemes
+              </p>
+            </div>
+            <div className="h-auto rounded-lg my-1 bg-slate-100 shadow-gray-300">
+              <img
+                src={uniimage}
+                className="w-full h-auto rounded-lg shadow-md"
+                alt=""
+              />
+              <p className="text-center mt-2 hover:text-blue-500">
+                Prime Location
+              </p>
+            </div>
+          </div>
+          <div className="w-full md-w-[48%] md:mt-24 ">
+            <img
+              src={uniimage}
+              className="w-full h-auto rounded-lg shadow-md my-1 bg-slate-100 shadow-gray-300"
+              alt=""
+            />
+            <p className="text-center mt-2 hover:text-blue-500">
+              Internship Facilities
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
