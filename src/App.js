fb4a1f7d48f94aac96d61b85f1103c8c1327dcec
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-07-15 13:26:10 +0545

    adderd the router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import {HashRouter as Router,Routes, Route} from 'react-router-dom'
+import { HomePage } from './components/HomePage/HomePage';
+import ContactUsPage from './components/ContactUsPage/ContactUsPage';
+import Header from './components/NavBar/Header';
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Router>
         <Routes>
-          <Route></Route>
+          <Route path='/' element={<HomePage/>}></Route>
+          <Route path='/contact-us' element={<ContactUsPage/>}></Route>
         </Routes>
-
       </Router>
     </div>
   );
---
+++ src/components/ContactUsPage/ContactUsPage.js
@@ -0,0 +1,8 @@
+function ContactUsPage(){
+    return(
+        <>
+        <h1>This is Contact Us Page</h1>
+        </>
+    )
+}
+export default ContactUsPage
